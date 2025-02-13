cf88d4622dae016cc01f57a652b06f930ea6c268
Jen2442 <collazojennifer22@yahoo.com>
2025-02-13 15:23:13 -0800

    Update vector.cpp

--- Lab_2/vector.cpp
+++ Lab_2/vector.cpp
@@ -1,1 +1,11 @@
-
+#include<iostream>
+#include<vector>
+using namespace std;
+/**
+ * @brief prints the elements in the vector and their memory locations
+ * 
+ * @param arr - vector of integer
+ */
+void printMemvec(){
+  
+}
