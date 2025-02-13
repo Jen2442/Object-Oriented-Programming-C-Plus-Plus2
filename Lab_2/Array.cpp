7e90eea9477ea67e5721a0bfc1c3c36e32c0b7b0
Jen2442 <collazojennifer22@yahoo.com>
2025-02-13 00:44:54 -0800

    First function
    First function

--- Lab_2/Array.cpp
+++ Lab_2/Array.cpp
@@ -1,2 +1,16 @@
 #include<iostream>
 #include<vector>
+using namespace std;
+/**
+ * @brief prints the elements in the array adn their memory locations
+ * 
+ * @param arr - array of integers
+ * @param size - the number of elements in the array
+ */
+void printMenArr(const int*arr,int size){
+    printf("Array - Each int is worth %lu bytes\n", sizeof(arr[0]));
+    for(int i=0; i<size;i++){
+        printf("Value :%i at Memory Location: %p\n", arr[i],arr+i);
+
+    }
+}
