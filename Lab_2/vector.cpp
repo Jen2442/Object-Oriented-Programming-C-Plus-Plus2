c23621b43038d77c4688a66527a38894baec97b9
Jen2442 <collazojennifer22@yahoo.com>
2025-02-14 15:16:31 -0800

    correct up til increment

--- Lab_2/vector.cpp
+++ Lab_2/vector.cpp
@@ -1,11 +1,50 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+
 /**
  * @brief prints the elements in the vector and their memory locations
  * 
  * @param arr - vector of integer
  */
-void printMemvec(){
-  
+void printMemvec(const int *vec, int size){// void printMemvec(const int *vec,int size)
+
+    cout<<"vector -each int is worth %lu bytes\n", sizeof(vec[0]);// not working
+
+    for(int i=0; i<size;i++){
+    printf("value: %i at memory location : %p\n", vec[i],vec+i);
+
+    }
+
 }
+  
+void incvecBy10(int*vec,int size){
+    for(int i=0;i<size;i++){
+        vec[i]+=10;
+
+    }
+}
+
+int main(){
+    const int SIZE=5;
+    int vec[SIZE];
+
+    for(int i=0;i<SIZE;i++){
+        vec[i]=100+i;
+
+    }
+
+    printf("before increment---------------\n");
+    printMemvec(vec,SIZE);
+    incvecBy10(vec,SIZE);
+    printf("after increment----------------\n");
+    printMemvec(vec,SIZE);
+
+
+
+    
+
+    return 0;
+
+}
+
