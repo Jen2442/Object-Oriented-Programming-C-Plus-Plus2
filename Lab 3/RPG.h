583da0804ebf4f88a09d627db837fbe1e30c3372
Jen2442 <collazojennifer22@yahoo.com>
2025-02-28 22:05:19 -0800

    Update RPG.h

--- Lab 3/RPG.h
+++ Lab 3/RPG.h
@@ -1,1 +1,26 @@
+#indef RPG_h
+#define RPG_h
+#include <iostream> 
+#include <string>
 
+using namespace std;
+
+const int SKILL_SIZE=2;
+
+Class RPG{
+
+    PUBLIC:
+    //CONSTRUCTORS
+    RPG();
+    RPG(string name, int health, int strentgh, int defense, string type);
+
+    string getname() const;
+    int gethealth() const;
+    int getstrength() const;
+    int getdefense() const;
+    string gettype() const;
+
+
+
+}
+   
