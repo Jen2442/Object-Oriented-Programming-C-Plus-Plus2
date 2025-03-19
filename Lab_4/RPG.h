4a56b43cea746066317e10e92b4c6f51029680ca
Jen2442 <collazojennifer22@yahoo.com>
2025-03-18 23:30:09 -0700

    HI

---
+++ Lab_4/RPG.h
@@ -0,0 +1,45 @@
+#ifndef RPG_H
+#define RPG_H
+#include <iostream> 
+#include <string>
+
+using namespace std;
+
+const int SKILL_SIZE=2;
+
+class RPG {
+
+    public:
+    /////////////////
+    RPG();
+    RPG(string name, int health, int strentgh, int defense, string type);
+/////////////////////
+    string getName() const;
+    int getHealth() const;
+    int getStrength() const;
+    int getDefense() const;
+    bool isAlive() const;
+    string getType() const;
+/////////
+    void setSkills();
+    void printAction(string,RPG);
+    void updateHealth(int);
+    void attack(RPG target);
+    void useSkill(RPG target, string skill);
+
+////////////////////////
+   private:
+
+   string type;
+   string skills [SKILL_SIZE];
+   string name;
+   int health;
+   int strngth;
+   int defense;
+
+
+   
+
+};
+   
+#endif
