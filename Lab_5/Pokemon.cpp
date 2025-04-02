2d4a754c064597d2e8d11d1834749d6530b843e0
Jen2442 <collazojennifer22@yahoo.com>
2025-04-02 16:10:12 -0700

    Charmander

--- Lab_5/Pokemon.cpp
+++ Lab_5/Pokemon.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
+#include "Pokemon.h"
 
+/**
+ * @brief construct a new pokemon :: pokemon object
+ * 
+ */
+Pokemon :: Pokemon(){
+   name= "unidentified";
+   hp=0;
+   attack=0;
+   defense=0;
+   cout<<"default constructor (Pokemon)\n";
+
+}
+/**
+ * @brief construct a new pokemon :: pokemon object
+ * 
+ * @param name
+ * @param hp
+ * @param att
+ * @param def
+ * @param type
+ * 
+ */
+Pokemon::Pokemon(string name, int hp, int att, int def, vector<string> type){
+    this->name=name;
+    this->hp=hp;
+    attack=att;
+    defense=def;
+    type=type;
+    cout<<"overloaded constructor (Pokemon)\n";
+
+}
+/**
+ * @brief says whatever this pokemon normally says
+ * 
+ */
+void Pokemon::speak(){
+    cout<<"...\n";
+}
+
+void Pokemon::printStats(){
+    printf("Name:%s\t HP: %i\t DEF: %i\t ATT: %i\n",name.c_str(),hp,defense,attack);
+    cout<<"Type: ";
+    for(int i=0;i<type.size();i++){
+        cout<<type[i]<<"\t";
+    }
+    cout<<endl;
+}
