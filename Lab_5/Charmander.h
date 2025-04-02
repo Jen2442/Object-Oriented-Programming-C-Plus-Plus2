2098c204bac9846095ed5887e8e12420eef9344d
Jen2442 <collazojennifer22@yahoo.com>
2025-04-02 01:16:57 -0700

    Charmander

--- Lab_5/Charmander.h
+++ Lab_5/Charmander.h
@@ -1,1 +1,20 @@
+#include <string>
+#include<vector>
+#include "Pokemon.h"
+using namespace std;
 
+class Charmander : public Pokemon{
+    public:
+    //--------------constructors-------------------
+
+ Charmander();
+ Charmander(string name,int hp, int att, int def, vector<string>t, vector<string> s );
+ //--------------------------Mutator functions-----------------
+void speak()/*override*/;
+ void printStats()/*override*/;
+
+ private:
+ vector<string> skills;
+ /*name,hp,attack,defense*/
+};
+#endif
